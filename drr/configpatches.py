5f9100741897f1a32e46370cdd24ec7cd6b86706
SophieHYe <He_Ye_90s@hotmail.com>
2019-03-08 16:06:20 +0100

    update drr patches configuration

--- drr/configpatches.py
+++ drr/configpatches.py
@@ -24,6 +24,7 @@ def travFolder(dir):
                         #create delta.txt file
                         targetfile=""
                         linenumber=""
+                        sourcefile=""
                         with open("./metadata.csv") as metadata:
                                 lines = metadata.readlines()
                                 for line in lines:
@@ -32,14 +33,81 @@ def travFolder(dir):
                                                 if bugId==data[1]:
                                                         targetfile=data[2].split('/')[-1]
                                                         linenumber=data[3].replace("\n","")
-
+                                                        sourcefile=data[2]
 
                         if targetfile=="":
                                 print projectId+bugId+" has NO INFO!!"
 
+                        patchlineNo=linenumber
+                        with open(os.path.join(dir, f)) as patch:
+                                lines = patch.readlines()
+                                line3=lines[2].split(" ")[1]
+                                line3=line3.split(",")[0]
+                                patchlineNo=line3.split("-")[1]
+
+
                         with open(patchfolder+"/delta.txt","w") as delta:
                                 delta.write("/drr"+bugfolder.split(".")[-1]+"/bug/"+targetfile+":"+linenumber+",1\n")
-                                delta.write("/drr"+patchfolder.split(".")[-1]+"/patch/"+targetfile+":"+linenumber+",1")
+                                delta.write("/drr"+patchfolder.split(".")[-1]+"/patch/"+targetfile+":"+patchlineNo+",1")
+                        #create oracle.txt file one for per bug!
+                        with open(bugfolder+"/oracle.txt","w") as oracle:
+                                oracle.write("/drr"+bugfolder.split(".")[-1]+"/fix/"+targetfile+":"+linenumber+",1\n")
+                        #create fix folder
+                        fixfolder=bugfolder+"/fix"
+                        if not os.path.exists(fixfolder):
+                                os.system("mkdir "+fixfolder)
+
+                         #create common bug for DIFFTGen folder
+                        cbugfolder=bugfolder+"/bug"
+                        if not os.path.exists(cbugfolder):
+                                os.system("mkdir "+cbugfolder)
+
+                         #create patch for DIFFTGen folder
+                        cpatchfolder=patchfolder+"/patch"
+                        if not os.path.exists(cpatchfolder):
+                                os.system("mkdir "+cpatchfolder)
+
+                        #checkout defects4j 
+                        
+                        d4jfolder=bugfolder+"/defects4j"
+                        if not os.path.exists(d4jfolder):
+                                os.system("cd  "+bugfolder);
+                                os.system("mkdir "+d4jfolder)
+                                os.system(d4jpath+"/defects4j checkout  -p "+projectId +"  -v "+bugId+"b  -w  "+d4jfolder+"/"+projectId+"_"+bugId+"_buggy");
+                                os.system(d4jpath+"/defects4j checkout  -p "+projectId +"  -v "+bugId+"f  -w  "+d4jfolder+"/"+projectId+"_"+bugId+"_fix");
+
+
+                        #copy bug file 
+                        shutil.copy(d4jfolder+"/"+projectId+"_"+bugId+"_buggy/"+sourcefile, cbugfolder+"/") 
+                        #copy patch file
+                        shutil.copy(d4jfolder+"/"+projectId+"_"+bugId+"_buggy/"+sourcefile, cpatchfolder+"/") 
+                        #copy fix file
+                        shutil.copy(d4jfolder+"/"+projectId+"_"+bugId+"_fix/"+sourcefile, fixfolder+"/") 
+                        #apply patch to patchfile
+                        os.system("patch -u -l --fuzz=10  -i   " +patchfolder+"/"+f +"   "+ cpatchfolder+"/"+targetfile)
+                        
+                        #compile the buggy file and keep the build classess
+                        os.chdir(d4jfolder+"/"+projectId+"_"+bugId+"_buggy")
+                        os.system(d4jpath+"/defects4j compile ")
+                        os.chdir("../../../../../")
+                        # os.chdir("/Users/sophie/Documents/DiffTGen/drr")
+                        if not os.path.exists(bugfolder+"/classes"):
+                                os.system("mkdir "+bugfolder+"/classes")                       
+
+                        if os.path.exists(d4jfolder+"/"+projectId+"_"+bugId+"_buggy/target"):
+                                os.system("cp -rf "+d4jfolder+"/"+projectId+"_"+bugId+"_buggy/target/classes/  "+bugfolder+"/classes") 
+                       
+                        elif os.path.exists(d4jfolder+"/"+projectId+"_"+bugId+"_buggy/build/classes"):
+                                os.system("cp -rf "+d4jfolder+"/"+projectId+"_"+bugId+"_buggy/build/classes/  "+ bugfolder+"/classes") 
+
+                        elif os.path.exists(d4jfolder+"/"+projectId+"_"+bugId+"_buggy/classes"):
+                                os.system("cp -rf "+d4jfolder+"/"+projectId+"_"+bugId+"_buggy/classes/  "+ bugfolder+"/classes") 
+
+                                #delete the fix files
+                        # if os.path.exists(d4jfolder):
+                        #         os.system("rm -rf  "+d4jfolder)
+
+
 
 
                         
@@ -50,5 +118,7 @@ def travFolder(dir):
 
                 
 if __name__ == '__main__':
+        #change to your defects4j patch
+        d4jpath="/Users/sophie/Documents/defects4j/framework/bin"
         travFolder("./D_correct_DS")
 
